7fc27086a55f1d35fe6397f1992952900e8de936
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-05-26 23:55:58 +0530

    Add 20.ValidParanthesis problem

---
+++ 20.ValidParanthesis.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool validParnatheses(string s)
+{
+    stack <char> st;
+    map<char,char> mp;
+    mp.insert(pair<char,char>(']','['));
+    mp.insert(pair<char,char>('}','{'));
+    mp.insert(pair<char,char>(')','('));
+    int i;
+    for(i=0;i<s.length();i++)
+    {
+        if(s[i]=='[' || s[i]=='{' || s[i]=='(')
+           st.push(s[i]);
+        else
+            {
+                if(!st.empty() && st.top()!=s[i])
+                   st.pop();
+                else
+                   return 0;
+                   
+            }
+    } 
+
+    return st.empty();
+    
+}
+
+int main()
+{
+    string s;
+    cout<<"Enter the parantheses : "<<endl;
+    cin>>s;
+    cout<<validParnatheses(s)<<endl;
+    return 1;
+}
