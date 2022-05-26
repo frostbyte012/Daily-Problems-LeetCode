4ffa83d4f8a0394a6cfc203e518e745f0e322c38
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-05-26 23:56:25 +0530

    Add 32.LongestValidParantheses problem

---
+++ 32.LongestValidParanthesisStack.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h> 
+using namespace std;
+bool longestValidParentheses(string s)
+{
+    stack <int> st;
+    st.push(-1);
+    int i,mx=0;
+    for(i=0;i<s.length();i++)
+    {
+        if(s[i]=='(')
+           st.push(i);
+        else{
+            st.pop();
+            if(st.empty())
+               st.push(i);
+            else
+               mx=max(mx,i-st.top());
+
+        }    
+    
+    }
+    return mx;
+}
+
+int main()
+{
+    string s;
+    cout<<"Enter the parentheses sequence :"<<endl;
+    cin>>s;
+    cout<<longestValidParentheses(s)<<endl;
+    return 1;
+}
+
