612402a32299a9e5bd9ecefee88db499d9f0a107
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-05-31 21:23:31 +0530

    build : Add 1461. Check If A String Conatains All Binary Codes of Size K problem.

---
+++ 1461.CheckIfAStringContainsAllBinaryCodesOfSizeK.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool hasAllCodes(string s, int k)
+{
+    int i=0;
+    if(s.length()<k)
+        return 0;
+    unordered_set <string> unique_sub_str;
+    for(i=0;i<=s.length()-k;i++)
+        unique_sub_str.insert(s.substr(i,k));
+    return unique_sub_str.size()==pow(2,k);
+}
+
+int main()
+{
+    int k;
+    string s;
+    cout<<"Enter the string and the size of the binary bits length."<<endl;
+    cin>>k>>s;
+    cout<<hasAllCodes(s,k)<<endl;
+    return 1;
+}
