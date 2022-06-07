41c7a7a2e6373fcb2d89c2b65625a009f4517c29
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-06-07 11:08:41 +0530

    build : Add N-QueensII Problem

---
+++ 52.N-QueensII.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class N_QueensII{
+public:
+    void solve(int col,vector<string>&board,vector<vector<string>>&ans,vector<int>&leftRow,vector<int>&upperDiagonal,vector<int>lowerDiagonal,int n){
+
+   if(col==n)
+   {
+       ans.push_back(board);
+       return;
+   } 
+
+   for(int row=0;row<n;row++)
+   {
+       if(leftRow[row]==0 && upperDiagonal[n-1 + col-row]==0 && lowerDiagonal[row+col]==0)
+       {
+           board[row][col]='Q';
+           leftRow[row]=1;
+           upperDiagonal[n-1+col-row]=1;
+           lowerDiagonal[row+col]=1;
+           solve(col+1,board,ans,leftRow,upperDiagonal,lowerDiagonal,n);
+           board[row][col]='.';
+           leftRow[row]=0;
+           upperDiagonal[n-1+col-row]=0;
+           lowerDiagonal[row+col]=0;
+       }
+   }
+
+
+}
+
+public:
+vector<vector<string>> solveNQueens(int n)
+{
+    vector<vector<string>> ans;
+    vector<string> board(n);
+    string s(n,'.');
+    for(int i=0;i<n;i++)
+    {
+        board[i]=s;
+    }
+
+    vector<int> leftRow(n,0),upperDiagonal(2*n-1,0),lowerDiagonal(2*n-1,0);
+    solve(0,board,ans,leftRow,upperDiagonal,lowerDiagonal,n);
+    return ans;
+}
+public:
+int totalNQueens(int n)
+{
+  vector<vector<string>> ans=solveNQueens(n);
+  return ans.size(); 
+}
+};
+
+int main()
+{
+    int n,solutions;
+    cout<<"Enter the value of N X N matrix, size of the board."<<endl;
+    cin>>n;
+    N_QueensII NQueensII;
+    solutions=NQueensII.totalNQueens(n);
+    if(n%2!=0)
+       {
+            cout<<"Enter an even number"<<endl;
+            return 0;
+       }
+ cout<<"The Unique number of solutions will be : "<<solutions<<endl;
+ return 1;
+}
