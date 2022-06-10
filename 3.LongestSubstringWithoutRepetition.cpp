0da0d56e8d71390ce51cd4fbab1c79aefdec35b1
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-06-11 02:05:30 +0530

    build : Add a 3. Longest Substring Without repetition problem.

---
+++ 3.LongestSubstringWithoutRepetition.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    int lengthOfLongestSubstring(string s)
+      {
+            int start=0,end=0,maxLen=0;
+            unordered_map<char,int> freq;
+            while(end<s.length()-1)
+            {
+                freq[s[end]]++;
+             
+                while(freq[s[end]]!=1)
+                {
+
+                      freq[s[start]]--;
+                      start++;
+
+                }
+
+            maxLen=max(maxLen,end-start+1);
+            end++;
+
+
+            }
+         
+         return maxLen;
+      
+
+      }
+};
+
+int main()
+{
+    string s;
+    cout<<"Enter the String "<<endl;
+    cin>>s;
+    Solution sol;
+    cout<<sol.lengthOfLongestSubstring(s)<<endl;
+    return 1;
+}
