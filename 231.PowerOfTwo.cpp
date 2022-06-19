b511a9d007697e5513d0fb5b857d91ce7b40bc77
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-06-19 14:38:44 +0530

    231. Power Of Two problem.

---
+++ 231.PowerOfTwo.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+     
+        for(int i=0;i<=31;i++)
+        {
+            if(pow(2,i)==n)
+                return true;
+        }
+        
+        return false;
+    }
+};
