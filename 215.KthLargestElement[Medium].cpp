a06415e0e6f58a826b4bdd6ca58d5ca3bf4973c2
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-06-23 02:13:46 +0530

    build : Add 215.Kth Largest Element problem

---
+++ 215.KthLargestElement[Medium].cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        
+        int n=nums.size();
+        
+        sort(nums.begin(),nums.end());
+        
+
+        return nums[n-k];
+        
+    }
+};
