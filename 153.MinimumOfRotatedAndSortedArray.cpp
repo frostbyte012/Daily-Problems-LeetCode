eba06d5bc6f0753bcff859531da5ee89e1ed27ed
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-06-28 03:07:06 +0530

    build : Add 153.Maximum of Rotated and Sorted Array .

---
+++ 153.MinimumOfRotatedAndSortedArray.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+
+public:
+    int findMin(vector<int>& nums) {
+    
+      sort(nums.begin(),nums.end());
+      return nums[0];
+
+    }
+};
