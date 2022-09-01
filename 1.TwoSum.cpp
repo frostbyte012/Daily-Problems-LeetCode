23455c81c83bc16bcdfefd4ba7d6a68d6388b63c
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-09-02 01:11:27 +0530

    fix : Update 1. Two Sum Problem.

--- 1.TwoSum.cpp
+++ 1.TwoSum.cpp
@@ -5,18 +5,39 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
+//         int i;
 
-        for(int i=0;i<nums.size()-1;i++)
+//         for(i=0;i<nums.size()-1;i++)
+//         {
+//            for(int j=i+1;j<nums.size();j++)
+//            {
+//                if(nums[i]+nums[j]==target)
+//                {
+//                    return {i,j};
+//                }
+//            }
+//         }
+        
+//        return {0,0}; 
+        
+        
+        
+// Optimised approach
+
+
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
         {
-           for(int j=i+1;j<nums.size();j++)
+           
+           if(mp.find(target-nums[i])!=mp.end())
            {
-               if(nums[i]+nums[j]==target)
-               {
-                   return {i,j};
-               }
+               return {mp[target-nums[i]],i};
            }
+            mp[nums[i]]=i;
         }
         
-       return {0,0}; 
-    }
+        return {};
+        
+                
+    }    
 };
