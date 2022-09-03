488f77602f76185377c2d1870baae738211bdd2f
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-09-03 15:31:53 +0530

    fix : Update 125. Valid Palindrome problem

--- 125.ValidPalindrome.cpp
+++ 125.ValidPalindrome.cpp
@@ -5,31 +5,51 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         
+//         string s1="",s2="";
+        
+        
+//         for(int i=0;i<s.length();i++)
+//         {
+//             if(isalnum(s[i]))
+//             {
+//               s1.push_back(s[i]);    
+//             }
+            
+//         }
+        
+//         transform(s1.begin(),s1.end(),s1.begin(),::tolower);
+        
+//         s2=s1;
+        
+//         reverse(s1.begin(),s1.end());
+        
+//         if(s2==s1)
+//         {
+//             return true;
+//         }
+        
+//             return false;
+        
+        
         string s1="",s2="";
-        
-        
         for(int i=0;i<s.length();i++)
         {
             if(isalnum(s[i]))
-            {
-              s1.push_back(s[i]);    
-            }
-            
+                s1.push_back(s[i]);
         }
         
         transform(s1.begin(),s1.end(),s1.begin(),::tolower);
         
-        s2=s1;
+        int i=0,j=s1.length()-1;
         
-        reverse(s1.begin(),s1.end());
-        
-        if(s2==s1)
+        while(i<s1.length() && j>=0)
         {
-            return true;
+            if(s1[i]!=s1[j])
+                return false;
+            j--;
+            i++;
         }
         
-            return false;
-        
-        
+        return true;
     }
 };
