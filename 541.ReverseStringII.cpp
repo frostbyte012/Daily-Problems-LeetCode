5688d3aee96217e60a157e3eca8177286720901b
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-09-22 17:49:27 +0530

    build  : Add Reverse String II problem.

---
+++ 541.ReverseStringII.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    
+public:
+    string reverseStr(string s, int k) {
+     
+        
+        for(int i=0;i<s.length();i+=2*k)
+        {
+            if(i+k<=s.length()-1)
+            {
+                reverse(s.begin()+i,s.begin()+i+k);
+            }
+            else{
+                reverse(s.begin()+i,s.end());
+            }
+        }
+        
+        return s;
+        
+    }
+};
