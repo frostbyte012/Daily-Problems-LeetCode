d1a293b3f9061377bef77fe194f51cbf443495fe
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-09-24 23:50:11 +0530

    fix : Update the docs and comments int he problem.

--- 76.MinimumWindowSubstring.cpp
+++ 76.MinimumWindowSubstring.cpp
@@ -7,19 +7,22 @@ public:
         
         unordered_map<char,int>mp;
         
+        // initialize frequency table for t
         for(auto c:t)
         {
             mp[c]++;
         }
         
+         // initialize sliding window
         int start=0,end=0,len=INT_MAX,counter=mp.size();
         
         string ans="";
         
+        // start sliding window
         while(end<s.length())
         {
             char end_char=s[end];
-            
+            // if current char found in map, decrement count
             if(mp.find(end_char)!=mp.end())
             {
                 mp[end_char]--;
@@ -31,15 +34,24 @@ public:
             }
             
             end++;
-            
+            // if counter == 0, means we found an answer, now try to trim that window by sliding begin to right.
             while(counter==0)
             {
+                // store new answer if smaller than previously best
                 if(end-start<len)
                 {
                     len=end-start;
                     
                     ans=s.substr(start,end-start);
                 }
+
+                 //begin char could be in table or not, 
+                // if not then good for us, it was a wasteful char and we shortened the previously found substring.
+                
+                // if found in table increment count in table, as we are leaving it out of window and moving ahead, 
+                // so it would no longer be a part of the substring marked by begin-end window
+                // table only has count of chars required to make the present substring a valid candidate
+                // if the count goes above zero means that the current window is missing one char to be an answer candidate  
               
               char start_char=s[start];  
                 
