355ddb83f3c333fb1c5447283cd6d862a3431ebc
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-09-26 20:23:41 +0530

    fix : Update 622. Design Circular Queue Problem for adding comments and docs.

--- 622.DesignCircularQueue.cpp
+++ 622.DesignCircularQueue.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+//class creating the Node for implementing the data and pointers together
 class MyCircularQueue {
     
 private:
     class Node{
+        //node having data and next pointer
         public:
             int data;
             Node* next;
@@ -22,6 +23,7 @@ private:
     
 public:
     MyCircularQueue(int k) {
+        //ensuring the initial value for si and max_si that acts as checkers 
         si=0;
         max_si=k;
         root=NULL;
@@ -29,14 +31,16 @@ public:
     }
     
     bool enQueue(int value) {
+        //function for dequeing 
         if(si==max_si)
-        {
+        {//checking if the queue is filled
             return false;
         }
         si++;
+        //creating the new node value
         Node* newNode=new Node(value);
         if(!root)
-        {
+        {//checking the root value if NULL or not
             root=newNode;
             rear=newNode;
             rear->next=root;
@@ -50,15 +54,19 @@ public:
     }
     
     bool deQueue() {
+        //dequeing function
         if(si==0){
+        //checking if the queue is empty or not
             return false;
         }
         si--;
         if(root==rear){
+            //checking if the root and rear are same?
             rear=NULL;
             root=NULL;
         }
         else{
+            //new Node created and initialized root and rear if the root and rear aren't same
             Node* newRoot=root->next;
             root=newRoot;
             rear->next=root;
@@ -68,6 +76,7 @@ public:
     }
     
     int Front() {
+        //func for checking the front data=
         if(!root){
             return -1;
         }
@@ -76,6 +85,7 @@ public:
     }
     
     int Rear() {
+        //checking the rear data
         if(!root){
             return -1;
         }
@@ -83,10 +93,12 @@ public:
     }
     
     bool isEmpty() {
+        //checking the queue is empty or not?
        return root==NULL; 
     }
     
     bool isFull() {
+        //checking if the queue is filled completely or not?
         return si==max_si;
     }
 };
