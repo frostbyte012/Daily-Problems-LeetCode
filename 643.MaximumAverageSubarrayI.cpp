b1aa681a52132df678dffc55f4aa209921ef9128
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-10-01 23:23:10 +0530

    build : Add Maximum Average Subarray I problem.

---
+++ 643.MaximumAverageSubarrayI.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+
+        double sum=0.0,max_sum=0.0;
+        
+        for(int i=0;i<k;i++)
+        {
+            sum+=nums[i];
+            max_sum=(max_sum,sum);
+        }
+
+        
+        for(int i=k;i<nums.size();i++)
+        {
+            sum+=nums[i]-nums[i-k];
+            max_sum=max(max_sum,sum);
+        }
+        
+        return max_sum/k;
+        
+    }
+};
