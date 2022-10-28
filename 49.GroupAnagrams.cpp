e51edb5627e6c0abc8f212eeeaf85db4e06fe03e
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-10-28 22:04:24 +0530

    build : Add 49. Group Anagrams problem.

--- 49.GroupAnagrams.cpp
+++ 49.GroupAnagrams.cpp
@@ -1,29 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
           
-        unordered_map<string,vector<string>> mp;
-        vector<vector<string>> ans;
+        
+        unordered_map<string,vector<string>>mp;
+        
+        vector<vector<string>>ans;
+        
         int i=0;
         
         while(i<strs.size())
         {
             string temp = strs[i];
+            
             sort(temp.begin(),temp.end());
+            
             mp[temp].push_back(strs[i]);
+            
             i++;
-           
+            
         }
         
-        for(auto it=mp.begin();it!=mp.end();it++)
+        for(auto t=mp.begin();t!=mp.end();++t)
         {
-            ans.push_back(it->second);
+            ans.push_back(t->second);
         }
         
         return ans;
+        
         
     }
 };
