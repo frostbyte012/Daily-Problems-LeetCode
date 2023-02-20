0db62db392b38d5edca111bc7b12763a0cea247b
Deepraj Majumdar <murieldeepraj@gmail.com>
2023-02-20 19:15:23 +0530

    fix : Update 35. Search Insert Position file.

--- 35.SearchInsertPosition.cpp
+++ 35.SearchInsertPosition.cpp
@@ -7,28 +7,35 @@ public:
 
     int searchInsert(vector<int>& nums, int target) {
 
-         int end=nums.size()-1,start=0;
+
+        int start=0,end=nums.size()-1; // initializing the start with zero index and end with the last index
         
-          while(start<=end)
-          {
-              int mid=(start+end)/2;
-              
-              if(nums[mid]==target)
-              {
-                  return mid;
-              }
-              
-              else if(nums[mid]<target)
-              {
-                 start=mid+1;
-              }
-              else
-              {
-                 end=mid-1;
-              }
-          }
+        while(start<=end)  // looping until we find the element and return it
+        {
+            int mid=(start+end)/2; // taking the avg mid of the end and the start element 
+            
+            if(nums[mid]==target) //if the mid index of the nums ,matches with the target then we return the position that's the mid 
+            {
+                return mid;    
+            }
+            
+            else if(nums[mid]<target) //if the mid index of the nums ,less than the target then we increase the start  position of the mid+1 
+            {
+                start=mid+1;
+            }
+            
+            else // if the mid index of the nums, greater than the target then we decrease the end by 1
+            {
+                end=mid-1;
+            }
+            
+        }
         
-         return start;
+        
+        return start; // finally returning the start if the position not find yet and we return the position to be inserted 
+        
         
     }
 };
+
+
