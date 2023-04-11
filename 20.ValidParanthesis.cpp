9a03e6a2605f91afcaec19afa193740dfe91a73c
Deepraj Majumdar <murieldeepraj@gmail.com>
2023-04-11 20:19:07 +0530

    fix : Update 20. Valid Parenthesis problem.

--- 20.ValidParanthesis.cpp
+++ 20.ValidParanthesis.cpp
@@ -1,36 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool validParnatheses(string s)
-{
-    stack <char> st;
-    map<char,char> mp;
-    mp.insert(pair<char,char>(']','['));
-    mp.insert(pair<char,char>('}','{'));
-    mp.insert(pair<char,char>(')','('));
-    int i;
-    for(i=0;i<s.length();i++)
-    {
-        if(s[i]=='[' || s[i]=='{' || s[i]=='(')
-           st.push(s[i]);
-        else
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack <char> st;
+        map<char,char> mp;
+        mp.insert(pair<char,char>(')','(')); // storing the alternate pair pf each stack element 
+        mp.insert(pair<char,char>(']','[')); // storing the alternate pair of each stack element
+        mp.insert(pair<char,char>('}','{'));// storing the alternative pair of each stack element
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') // if the element char appears to be opening brackets it pushes into the stack
+                st.push(s[i]);
+            else
             {
-                if(!st.empty() && st.top()==mp[s[i]])
-                   st.pop();
-                else
-                   return false;
-                   
+                if(!st.empty() && st.top()==mp[s[i]]) // else checks if the stack is empty and the element is closing bracket's alternative if so then pops the element   
+                    st.pop();
+                
+                else // else returns false;
+                    return 0;
+                
             }
-    } 
-
-    return st.empty();
-    
-}
-
-int main()
-{
-    string s;
-    cout<<"Enter the parantheses : "<<endl;
-    cin>>s;
-    cout<<validParnatheses(s)<<endl;
-    return 1;
-}
+        }
+        
+        return st.empty(); // finally it returns true or false storing the elements or manipulating them
+    }
+};
